4546700f6651f26da5b7ed479285a8d7184efe98
Eric Drechsel <eric@pdxhub.org>
2021-02-07 14:35:45 -0800

    .

--- profiles/hardware.nix
+++ profiles/hardware.nix
@@ -3,13 +3,12 @@
 {
   # for hardware / bare metal systems
 
+  # TODO(eric): split out hard drive profile
   # check S.M.A.R.T status of all disks and notify in case of errors
   services.smartd = {
     enable = true;
     notifications = {
       mail.enable = if config.services.postfix.enable then true else false;
-      x11.enable = if config.services.xserver.enable then true else false;
-      wall.enable = false;
       #test = true;
     };
   };
