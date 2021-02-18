92a7e74b46fd3fedec16f32d4979ed8e3e58ee46
Eric Drechsel <eric@pdxhub.org>
2021-02-18 01:53:55 -0800

    add empty silversurfer

--- flake.nix
+++ flake.nix
@@ -1,9 +1,15 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: {
-     nixosConfigurations.pidrive = nixpkgs.lib.nixosSystem {
-       system = "aarch64-linux";
-       modules = [ ./machines/pidrive/configuration.nix ];
+     nixosConfigurations = {
+       pidrive = nixpkgs.lib.nixosSystem {
+         system = "aarch64-linux";
+         modules = [ ./machines/pidrive/configuration.nix ];
+       };
+       silversurfer = nixpkgs.lib.nixosSystem {
+         system = "aarch64-linux";
+         modules = [ ./machines/pidrive/configuration.nix ];
+       };
      };
   };
 }
