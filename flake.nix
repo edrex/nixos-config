a5278d4c3a0d0a27d0b2af0332553284e60e4b0d
Eric Drechsel <eric@pdxhub.org>
2021-04-07 15:21:50 -0700

    borrow colemickens' mkSystem passing inputs

--- flake.nix
+++ flake.nix
@@ -1,23 +1,29 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     nixos-hardware.url = "path:./nixos-hardware"; # TODO: this is temp while I'm working on a branch
+
+    inputs.agenix.url = "github:ryantm/agenix";
+    inputs.agenix.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = { self, ... }@inputs: {
-     nixosConfigurations = {
-       pidrive = inputs.nixpkgs.lib.nixosSystem {
-         system = "aarch64-linux";
-         modules = [ ./machines/pidrive/configuration.nix ];
-       };
-       silversurfer = inputs.nixpkgs.lib.nixosSystem {
-         system = "x86_64-linux";
-         modules = [
-           ./machines/silversurfer/configuration.nix
-           inputs.nixos-hardware.nixosModules.apple-macbook-pro-2-2
-         ];
-       };
-     };
+  outputs = inputs:
+    let
+      mkSystem = pkgs: system: hostname:
+        pkgs.lib.nixosSystem {
+          system = system;
+          modules = [(./. + "/hosts/${hostname}/configuration.nix")];
+          specialArgs = { inherit inputs; };
+        };
+    in {
+      nixosConfigurations = {
+        pidrive = mkSystem inputs.nixpkgs "aarch64-linux" "pidrive";
+        silversurfer = mkSystem inputs.nixpkgs "x86_64-linux" "silversurfer";
+        #TODO: inputs.nixos-hardware.nixosModules.apple-macbook-pro-2-2
+      };
+    };
   };
 }
 #TODO: maybe map machine configs to outputs like https://www.reddit.com/r/NixOS/comments/j4k2zz/does_anyone_use_flakes_to_manage_their_entire/g7jrqcn/?context=3
