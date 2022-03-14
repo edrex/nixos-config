aa2455525c07d25231fd155f08b1f71c228d47a5
Eric Drechsel <eric@pdxhub.org>
2022-03-14 15:25:48 -0700

    edid WIP
    see https://github.com/edrex/custom-edids

--- hosts/chip/edid.nix
+++ hosts/chip/edid.nix
@@ -1,14 +1,17 @@
 { config, lib, pkgs, modulesPath, ... }:
 # https://discourse.nixos.org/t/how-to-tell-kernel-to-use-a-custom-modeline/5500
 let 
+  dest = "lib/firmware/edid";
+  path = "${dest}/chipedp.bin";
   # package for edid firmware
-  chip_edid = pkgs.runCommand "chip_edid" {} ''
-    mkdir -p $out/lib/firmware/edid
-    cp ${./chipedp.bin} $out/lib/firmware/edid/chipedp.bin
+  chip_edid = pkgs.runCommand filename {} ''
+    mkdir -p $out/${dest}
+    cp ${./${filename}} $out/${dest}/${filename}
   '';
 in {
   # add pkg to firmware
   hardware.firmware = [ chip_edid ];
+  boot.initrd.extraFiles."${path}".source = pkgs.runCommandLocal "${path}" {} "cp ${./chipedp.bin} $out";
   # add kernel param
   boot.kernelParams = [ "drm.edid_firmware=eDP-1:edid/chipedp.bin" ];
 }
